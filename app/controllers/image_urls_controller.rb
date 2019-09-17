de3088cb8dc104e5bab08e3455dfd73a5e7b3b77
Keith Long <keith.long@appfolio.com>
2019-09-17 13:56:12 -0700

    kl - image model, save links, update views

--- app/controllers/image_urls_controller.rb
+++ app/controllers/image_urls_controller.rb
@@ -1,7 +1,32 @@
 class ImageUrlsController < ApplicationController
-  def new
+  def index
+    @image_urls = ImageUrl.all
   end
+
+  def show
+    @image_url = ImageUrl.find(params[:id])
+  end
+
+  def new
+    @image_url = ImageUrl.new
+  end
+
   def create
-    render plain: params[:article].inspect
+    @image_url = ImageUrl.new(url_params)
+     # puts 'ddddddddddddddddd'
+     # puts @image_url.inspect
+    if @image_url.save
+      redirect_to @image_url
+    else
+      # render does not reissue a request (link_to does)
+      render 'new'
+
+    end
+  end
+
+  private
+
+  def url_params
+    params.require(:image_url).permit(:url)
   end
 end
