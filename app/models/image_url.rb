15a90ce667c35652725320526077ceb948532768
Keith Long <keith.long@appfolio.com>
2019-09-17 13:56:12 -0700

    kl - image model, save links, update views

--- app/models/image_url.rb
+++ app/models/image_url.rb
@@ -1,2 +1,6 @@
 class ImageUrl < ApplicationRecord
+  include ActiveModel::Validations
+  validates :url, presence: true
+  validates :url, url: true
+  validates :url, format: {with: /\.(png|jpg|jpeg)\Z/i}
 end
