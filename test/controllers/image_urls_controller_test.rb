80fdb0058c07f305ad9012dcca0e5783694a8faf
Keith Long <keith.long@appfolio.com>
2019-09-26 09:13:59 -0700

    kl - delete test

--- test/controllers/image_urls_controller_test.rb
+++ test/controllers/image_urls_controller_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class ImageUrlsControllerTest < ActionDispatch::IntegrationTest
+
+  def test_destroy
+    imageUrl = ImageUrl.create!(url: 'https://bcefiles.allaboutbirds.org/home/wp-content/uploads/2019/05/IBunting-Kinderman-58737651-FI.gif')
+    assert_difference('ImageUrl.count', -1) do
+      delete image_url_path(imageUrl.id)
+    end
+    assert_redirected_to image_urls_path
+    assert_nil ImageUrl.find_by(id: imageUrl.id)
+  end
+
 end
